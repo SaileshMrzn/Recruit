50fe906c4f40b33385a453eb793ab00c2cfd7ac6
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-02-03 00:17:58 +0545

    shadcn config and navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+import { navLinks } from "@/constants/navLinks";
+import { Icons } from "@/constants/icons";
+
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center p-2 lg:px-8 lg:py-4 bg-white">
+      {/* logo */}
+      <Icons.Logo className="w-36 h-auto lg:w-52" />
+      {/* nav links */}
+      <ul className="hidden lg:flex w-1/2 lg:justify-between">
+        {navLinks.map((nav, index) => (
+          <Link href={nav.link} key={index}>
+            <li>{nav.title}</li>
+          </Link>
+        ))}
+      </ul>
+
+      <HiOutlineMenuAlt3 size={30} className="lg:hidden" />
+
+      {/* buttons */}
+      <div className="gap-6 hidden lg:flex">
+        <Button size="lg" variant="outline">
+          Login
+        </Button>
+        <Button size="lg" className="text-white">
+          Signup
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    title: "Home",
+    link: "/home",
+  },
+  {
+    title: "Explore Jobs",
+    link: "/home",
+  },
+  {
+    title: "Trainings",
+    link: "/home",
+  },
+  {
+    title: "Why Recruit Nepal",
+    link: "/home",
+  },
+  {
+    title: "Contact Us",
+    link: "/contact",
+  },
+];
