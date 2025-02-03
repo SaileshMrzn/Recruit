9b2b3f167722c0be0a3f0d9fa5fb4168860cf23a
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-02-03 21:28:45 +0545

    navbar and job number spacing fix

--- src/components/JobsNumber.tsx
+++ src/components/JobsNumber.tsx
@@ -3,7 +3,7 @@ import JobSearchBar from "./JobSearchBar";
 
 const JobsNumber = () => {
   return (
-    <div className="bg-[#F2F9FD] flex flex-col items-center justify-center py-16 gap-8 px-6 lg:py-16">
+    <div className="bg-[#F2F9FD] flex flex-col items-center justify-center py-16 gap-8 px-6 lg:px-16">
       <div className="flex flex-col items-center gap-4">
         <p className="text-4xl font-semibold text-primary">
           105 Jobs Available
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,10 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center p-2 lg:px-8 lg:py-4 bg-white">
       {/* logo */}
-      <Logos.Logo_SM className="lg:hidden" />
-      <Logos.Logo className="hidden lg:block" />
+      <div className="min-w-56">
+        <Logos.Logo_SM className="lg:hidden" />
+        <Logos.Logo className="hidden lg:block" />
+      </div>
       {/* nav links */}
       <nav className="hidden lg:flex w-1/2 lg:justify-between">
         {navLinks.map((nav, index) => (
@@ -29,37 +31,39 @@ const Navbar = () => {
         ))}
       </nav>
 
-      <Sheet>
-        <SheetTrigger>
-          <Icons.MenuIcon className="lg:hidden" />
-        </SheetTrigger>
-        <SheetContent>
-          <SheetTitle>Menu</SheetTitle>
-          <nav className="flex flex-col mt-6 gap-4">
-            {navLinks.map((nav, index) => (
-              <SheetClose key={index} asChild>
-                <Link
-                  href={nav.link}
-                  className="text-lg font-medium hover:text-primary"
-                >
-                  {nav.title}
-                </Link>
-              </SheetClose>
-            ))}
-          </nav>
-          <div className="flex gap-6 mt-6">
-            <Button size="lg" variant="outline">
-              Login
-            </Button>
-            <Button size="lg" className="text-white">
-              Signup
-            </Button>
-          </div>
-        </SheetContent>
-      </Sheet>
+      <div className="lg:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Icons.MenuIcon className="lg:hidden" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetTitle>Menu</SheetTitle>
+            <nav className="flex flex-col mt-6 gap-4">
+              {navLinks.map((nav, index) => (
+                <SheetClose key={index} asChild>
+                  <Link
+                    href={nav.link}
+                    className="text-lg font-medium hover:text-primary"
+                  >
+                    {nav.title}
+                  </Link>
+                </SheetClose>
+              ))}
+            </nav>
+            <div className="flex gap-6 mt-6">
+              <Button size="lg" variant="outline">
+                Login
+              </Button>
+              <Button size="lg" className="text-white">
+                Signup
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
 
       {/* buttons */}
-      <div className="gap-6 hidden lg:flex">
+      <div className="gap-6 hidden lg:flex min-w-56">
         <Button size="lg" variant="outline">
           Login
         </Button>
