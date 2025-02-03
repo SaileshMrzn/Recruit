3fc0245ade84a89c6d01f6b1a196151417ac3606
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-02-03 11:16:12 +0545

    add sheet for mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 import { navLinks } from "@/constants/navLinks";
-import { Logos } from "@/constants/icons";
+import { Logos, Icons } from "@/constants/icons";
 
 import { Button } from "./ui/button";
 
@@ -11,17 +18,45 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center p-2 lg:px-8 lg:py-4 bg-white">
       {/* logo */}
-      <Logos.Logo_SM />
+      {/* <Logos.Logo_SM /> */}
+      <p>[logo]</p>
       {/* nav links */}
-      <ul className="hidden lg:flex w-1/2 lg:justify-between">
+      <nav className="hidden lg:flex w-1/2 lg:justify-between">
         {navLinks.map((nav, index) => (
           <Link href={nav.link} key={index}>
-            <li>{nav.title}</li>
+            {nav.title}
           </Link>
         ))}
-      </ul>
+      </nav>
 
-      <HiOutlineMenuAlt3 size={30} className="lg:hidden" />
+      <Sheet>
+        <SheetTrigger>
+          <Icons.MenuIcon className="lg:hidden" />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetTitle>Menu</SheetTitle>
+          <nav className="flex flex-col mt-6 gap-4">
+            {navLinks.map((nav, index) => (
+              <SheetClose key={index} asChild>
+                <Link
+                  href={nav.link}
+                  className="text-lg font-medium hover:text-primary"
+                >
+                  {nav.title}
+                </Link>
+              </SheetClose>
+            ))}
+          </nav>
+          <div className="flex gap-6 mt-6">
+            <Button size="lg" variant="outline">
+              Login
+            </Button>
+            <Button size="lg" className="text-white">
+              Signup
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       {/* buttons */}
       <div className="gap-6 hidden lg:flex">
